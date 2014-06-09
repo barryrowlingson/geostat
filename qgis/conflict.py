cf6bcf3b9bdff38e95085eb1551790d80df089bc
barryrowlingson <b.rowlingson@lancs.ac.uk>
2014-06-09 22:03:13 +0100

    Create conflict.py
    conflict overlap detection processing script

---
+++ qgis/conflict.py
@@ -0,0 +1,46 @@
+##[BSR scripts]=group
+##areas=vector
+##owner=field areas
+##conflicts=output vector
+
+from PyQt4.QtCore import *
+from qgis.core import *
+
+from processing.tools import vector
+import processing
+from processing.core.VectorWriter import VectorWriter
+
+areas_layer = processing.getObject(areas)
+areas_features = processing.features(areas_layer)
+attrid = areas_layer.fieldNameIndex(owner)
+
+fields={}
+
+POLYGON=3
+writer = VectorWriter(conflicts, None, fields, POLYGON, areas_layer.crs() )
+
+aFeat = QgsFeature()
+bFeat = QgsFeature()
+outFeat = QgsFeature()
+aGeom = QgsGeometry()
+bGeom = QgsGeometry()
+
+all_features = list(areas_features)
+n_features = len(all_features)
+for ia in range(n_features):
+    aFeat = all_features[ia]
+    aGeom = QgsGeometry(aFeat.geometry())
+    atMapA = aFeat.attributes()[attrid]
+    for ib in range(ia+1, n_features):
+        bFeat = all_features[ib]
+        atMapB = bFeat.attributes()[attrid]
+        if atMapA != atMapB:
+            bGeom = QgsGeometry(bFeat.geometry())
+            if aGeom.intersects(bGeom):
+                int_geom = QgsGeometry(aGeom.intersection(bGeom))
+                outFeat.setGeometry(int_geom)
+                writer.addFeature(outFeat)
+    
+del writer
+
+
